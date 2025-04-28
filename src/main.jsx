4b3836776e9f66322ded04d76b7bf01d4c7f3203
J0maa <jomaasaeed59@gmail.com>
2025-04-28 19:31:48 +0300

    Add all updated project files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'  // استيراد ملفات CSS
+import App from './App'  // استيراد مكون App
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// ربط تطبيق React بـ DOM
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
